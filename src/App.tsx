e0a9499fd07411b36d8d359269678e3c088f5c5e
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-04-08 23:37:39 -0400

    Created ExpenseList component and added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import ExpenseList from "./components/ExpenseList"
 
 
 function App() {
+  
+  const expenses = [
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]
 
   return (
     <div>
-    
+      <ExpenseList expenses={expenses} onDelete={(id) => console.log("Delete", id)} />
     </div>
   )
 }
